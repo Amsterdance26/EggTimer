14e22a9336275e2d83434c25d9538c4a98714df6
Amsterdance26 <amsterdance26@gmail.com>
2023-01-25 15:50:05 +0700

    Change

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        if softTime == 5 {
+            print("5")
+            }
+        if mediumTime == 7 {
+            print("7")
+        }
+    }
 
 }
