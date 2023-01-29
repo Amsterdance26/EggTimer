eb1df5200f0bc7305b20054780f45b9dba895a20
Amsterdance26 <amsterdance26@gmail.com>
2023-01-29 14:06:03 +0700

    Untill add sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,19 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var progressBar: UIProgressView!
 
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    @IBOutlet weak var titleLable: UILabel!
+
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var timer = Timer()
+    var totalTime = 0
+    var secondPassed = 0
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitl
-        if softTime == 5 {
-            print("5")
-            }
-        if mediumTime == 7 {
-            print("7")
-        }
+
+
+
+        timer.invalidate()
+
+        let hardness = sender.currentTitle!
+
+        totalTime = eggTimes[hardness]!
+
+        progressBar.progress = 0.0
+        secondPassed = 0
+        titleLable.text = hardness
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
 
+    @objc func updateTimer() {
+        if secondPassed < totalTime {
+            secondPassed += 1
+            progressBar.progress = Float(secondPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            titleLable.text = "DONE!"
+        }
+    }
 }
